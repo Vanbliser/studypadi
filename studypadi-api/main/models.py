91230bd0c9d556fc102000b69a87e691a98cb97f
Blossom Israel Ayogu <ayogublossomisrael@gmail.com>
2024-11-15 19:56:51 +0100

    Added main endpoints

--- studypadi-api/main/models.py
+++ studypadi-api/main/models.py
@@ -10,27 +10,41 @@ def get_super_user():
 class Module(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=get_super_user)
+    class Meta:
+        ordering = ['-created_at']
 
 class Submodule(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     module_id = models.ForeignKey(Module, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=get_super_user)
+    class Meta:
+        ordering = ['-created_at']
 
 class Section(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     submodule_id = models.ForeignKey(Submodule, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=get_super_user)
+    class Meta:
+        ordering = ['-created_at']
 
 class Topic(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     section_id = models.ForeignKey(Section, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=get_super_user)
+    class Meta:
+        ordering = ['-created_at']
 
 class Question(models.Model):
     difficulties = [
@@ -50,8 +64,11 @@ class Question(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=get_super_user)
     question_type = models.CharField(max_length=3, choices=question_types)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     difficulty = models.CharField(max_length=3, default="EAS", choices=difficulties)
     question = models.TextField()
+    class Meta:
+        ordering = ['-created_at']
 
 class Option(models.Model):
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -66,8 +83,11 @@ class Quiz(models.Model):
     topic_id = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     num_of_questions = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=get_super_user)
     questions = models.ManyToManyField(Question)
+    class Meta:
+        ordering = ['-created_at']
                                                  
 class Quiz_attempt(models.Model):
     statuses = [
@@ -82,7 +102,7 @@ class Quiz_attempt(models.Model):
     time_taken = models.DateTimeField(auto_now_add=True)
     
     class Meta:
-        ordering = ["time_taken"]
+        ordering = ["-time_taken"]
 
 class Response(models.Model):
     quiz_attempt = models.ForeignKey(Quiz_attempt, on_delete=models.CASCADE)
--- studypadi-api/main/serializers.py
+++ studypadi-api/main/serializers.py
@@ -2,58 +2,190 @@ from rest_framework import serializers
 from .models import Module, Submodule, Section, Topic, Question, Quiz, Quiz_attempt, Response, Option
 from account.models import User
 
-class CreateQuestionSerializer(serializers.Serializer):
+
+class ModuleSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    title = serializers.CharField()
+    description = serializers.CharField()
 
     class Meta:
-        fields = ['module_id', 'submodule_id', 'section_id', 'topic_id', 'difficulty', 'question']
+        model = Module
+        fields = ['id', 'title', 'description']
         extra_kwargs = {
-            'question': {'required': True}
+            'title': {'required': True},
+            'description': {'required': True},
         }
     
     def validate(self, attrs):
+        # check for additional fields
         allowed_fields = set(self.fields.keys())
+        for initial_data in self.initial_data:
+            extra_fields = set(initial_data.keys()) - allowed_fields
+            if extra_fields:
+                raise serializers.ValidationError("Bad request. Unknown field(s).")
+        return attrs
 
-        # Check for any additional fields
-        # extra_fields = set(self.initial_data.keys()) - allowed_fields
-        # if extra_fields:
-        #     raise serializers.ValidationError("Bad request. Unknown field(s).")
-        # try:
-        #     question = attrs.get('question')
-        # except:
-        #     raise serializers.ValidationError("Invalid JSON")
-        module_id = int(attrs.get('module_id', 0))
-        submodule_id = int(attrs.get('submodule_id', 0))
-        section_id = int(attrs.get('section_id', 0))
-        topic_id = int(attrs.get('topic_id', 0))
+class SubmoduleSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    title = serializers.CharField()
+    description = serializers.CharField()
+    module_id = serializers.IntegerField()
+
+    class Meta:
+        model = Submodule
+        fields = ['id', 'module_id', 'title', 'description']
+        extra_kwargs = {
+            'module_id': {'required': True},
+            'title': {'required': True},
+            'description': {'required': True},
+        }
+    
+    def validate(self, attrs):
+        # check for additional fields
+        allowed_fields = set(self.fields.keys())
+        for initial_data in self.initial_data:
+            extra_fields = set(initial_data.keys()) - allowed_fields
+            if extra_fields:
+                raise serializers.ValidationError("Bad request. Unknown field(s).")
+        print(attrs)
+        return attrs
+    
+class SectionSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    title = serializers.CharField()
+    description = serializers.CharField()
+    submodule_id = serializers.IntegerField()
+
+    class Meta:
+        model = Section
+        fields = ['id', 'submodule_id', 'title', 'description']
+        extra_kwargs = {
+            'submodule_id': {'required': True},
+            'title': {'required': True},
+            'description': {'required': True},
+        }
+    
+    def validate(self, attrs):
+        # check for additional fields
+        allowed_fields = set(self.fields.keys())
+        for initial_data in self.initial_data:
+            extra_fields = set(initial_data.keys()) - allowed_fields
+            if extra_fields:
+                raise serializers.ValidationError("Bad request. Unknown field(s).")
+        return attrs
+
+class TopicSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    title = serializers.CharField()
+    description = serializers.CharField()
+    section_id = serializers.IntegerField()
+
+    class Meta:
+        model = Topic
+        fields = ['id', 'section_id', 'title', 'description']
+        extra_kwargs = {
+            'section_id': {'required': True},
+            'title': {'required': True},
+            'description': {'required': True},
+        }
+    
+    def validate(self, attrs):
+        # check for additional fields
+        allowed_fields = set(self.fields.keys())
+        for initial_data in self.initial_data:
+            extra_fields = set(initial_data.keys()) - allowed_fields
+            if extra_fields:
+                raise serializers.ValidationError("Bad request. Unknown field(s).")
+        return attrs
+
+class OptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Option
+        fields = ['option', 'is_answer']
+        extra_kwargs = {
+            'option': {'required': True},
+            'is_answer': {'required': True}
+        }
+    def validate(self, attrs):
+        if isinstance(attrs['is_answer'], bool) or attrs['option'] != "":
+            return attrs
+        else:
+            raise serializers.ValidationError("Invalid options JSON")
+
+class CreateQuestionSerializer(serializers.Serializer):
+    module_id = serializers.IntegerField()
+    submodule_id = serializers.IntegerField()
+    section_id = serializers.IntegerField()
+    topic_id = serializers.IntegerField()
+    difficulty = serializers.CharField(max_length=3)
+    question = serializers.CharField()
+    options = OptionSerializer(many=True, write_only=True)
+
+    class Meta:
+        fields = ['module_id', 'submodule_id', 'section_id', 'topic_id', 'difficulty', 'question', 'options']
+        extra_kwargs = {
+            'question': {'required': True},
+            'options': {'required': True}
+        }
+
+    def validate(self, attrs):
+
+        module_id = attrs.get('module_id', None)
+        submodule_id = attrs.get('submodule_id', None)
+        section_id = attrs.get('section_id', None)
+        topic_id = attrs.get('topic_id', None)
         difficulty = attrs.get('difficulty', None)
-        if not Module.objects.filter(id=module_id).exists():
-            module_id = None
-        if not Submodule.objects.filter(id=submodule_id).exists():
-            submodule_id = None
-        if not Section.objects.filter(id=section_id).exists():
-            section_id = None
-        if not Topic.objects.filter(id=topic_id).exists():
-            topic_id = None
+        question = attrs.get('question', None)
+        
+        module = Module.objects.filter(id=module_id).first()
+        submodule = Submodule.objects.filter(id=submodule_id).first()
+        section = Section.objects.filter(id=section_id).first()
+        topic = Topic.objects.filter(id=topic_id).first()
         if difficulty not in ['EAS', 'HRD', 'MED']:
             difficulty = "EAS"
-        if attrs.get('question') == "":
+
+        if question == "":
             raise serializers.ValidationError("Bad request. empty question")
         
+        return_data = dict()
+        if module:
+            return_data['module'] = module.title
+        if submodule:
+            return_data['submodule'] = submodule.title
+        if section:
+            return_data['section'] = section.title
+        if topic:
+            return_data['topic'] = topic.title
+        return_data['difficulty'] = difficulty
+        return_data['question'] = question
+
+        print('return data', return_data)
+
         data = {
-            'module_id': module_id,
-            'submodule_id': submodule_id,
-            'section_id': section_id,
-            'topic_id': topic_id,
+            'module_id': module,
+            'submodule_id': submodule,
+            'section_id': section,
+            'topic_id': topic,
             'difficulty': difficulty,
-            'question': attrs.get('question')
+            'question': question,
+            'options': attrs.get('options'),
+            'return': return_data
         }
-        cleaned_data = {key: value for key, value in data.items() if value not in (0, None)}
-
-        print(cleaned_data)
+        cleaned_data = {k: v for k, v in data.items() if v is not None}
         return cleaned_data
+    
+    def to_representation(self, instance):
+        """
+        Override to include options in the response
+        """
+        representation = super().to_representation(instance)
+        representation['options'] = OptionSerializer(
+            Option.objects.filter(question=instance),
+            many=True
+        ).data
+        return representation
 
-class CreateQuestionsSerializer(serializers.ListSerializer):
-    print('validating')
+class CreateQuestionListSerializer(serializers.ListSerializer):
     child = CreateQuestionSerializer()
 
 class GenerateQuizSerializer(serializers.Serializer):
